cd44c661b86e1d7da52477a1529905d8a6f2b98d
Thijmen L <thijmen.langendam@migrato.nl>
2022-02-09 12:19:37 +0100

    Convert to WinForms applicatio

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WFC4All
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultPB = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize) (this.resultPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // resultPB
+            // 
+            this.resultPB.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.resultPB.Location = new System.Drawing.Point(868, 164);
+            this.resultPB.Name = "resultPB";
+            this.resultPB.Size = new System.Drawing.Size(400, 400);
+            this.resultPB.TabIndex = 0;
+            this.resultPB.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.resultPB);
+            this.Name = "Form1";
+            this.Text = "WFC4All";
+            ((System.ComponentModel.ISupportInitialize) (this.resultPB)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.PictureBox resultPB;
+
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,74 +1,81 @@
-﻿/*
-The MIT License(MIT)
-Copyright(c) mxgmn 2016.
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-The software is provided "as is", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the software.
-*/
-
-using System;
-using System.Xml.Linq;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
 
-static class Program
+namespace WFC4All
 {
-    static void Main()
+    static class Program
     {
-        Stopwatch sw = Stopwatch.StartNew();
-
-        Random random = new Random();
-        XDocument xdoc = XDocument.Load("samples.xml");
-
-        foreach (XElement xelem in xdoc.Root.Elements("overlapping", "simpletiled"))
+        [STAThread]
+        static void Main()
         {
-            Model model;
-            string name = xelem.Get<string>("name");
-            Console.WriteLine($"< {name}");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        
+        static void RunWfc()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
 
-            bool isOverlapping = xelem.Name == "overlapping";
-            int size = xelem.Get("size", isOverlapping ? 48 : 24);
-            int width = xelem.Get("width", size);
-            int height = xelem.Get("height", size);
-            bool periodic = xelem.Get("periodic", false);
-            string heuristicString = xelem.Get<string>("heuristic");
-            var heuristic = heuristicString == "Scanline" ? Model.Heuristic.Scanline : (heuristicString == "MRV" ? Model.Heuristic.MRV : Model.Heuristic.Entropy);
+            Random random = new Random();
+            XDocument xdoc = XDocument.Load("samples.xml");
 
-            if (isOverlapping)
+            foreach (XElement xelem in xdoc.Root.Elements("overlapping", "simpletiled"))
             {
-                int N = xelem.Get("N", 3);
-                bool periodicInput = xelem.Get("periodicInput", true);
-                int symmetry = xelem.Get("symmetry", 8);
-                int ground = xelem.Get("ground", 0);
+                Model model;
+                string name = xelem.Get<string>("name");
+                Console.WriteLine($"< {name}");
 
-                model = new OverlappingModel(name, N, width, height, periodicInput, periodic, symmetry, ground, heuristic);
-            }
-            else
-            {
-                string subset = xelem.Get<string>("subset");
-                bool blackBackground = xelem.Get("blackBackground", false);
+                bool isOverlapping = xelem.Name == "overlapping";
+                int size = xelem.Get("size", isOverlapping ? 48 : 24);
+                int width = xelem.Get("width", size);
+                int height = xelem.Get("height", size);
+                bool periodic = xelem.Get("periodic", false);
+                string heuristicString = xelem.Get<string>("heuristic");
+                var heuristic = heuristicString == "Scanline" ? Model.Heuristic.Scanline : (heuristicString == "MRV" ? Model.Heuristic.MRV : Model.Heuristic.Entropy);
 
-                model = new SimpleTiledModel(name, subset, width, height, periodic, blackBackground, heuristic);
-            }
-
-            for (int i = 0; i < xelem.Get("screenshots", 2); i++)
-            {
-                for (int k = 0; k < 10; k++)
+                if (isOverlapping)
                 {
-                    Console.Write("> ");
-                    int seed = random.Next();
-                    bool success = model.Run(seed, xelem.Get("limit", -1));
-                    if (success)
+                    int N = xelem.Get("N", 3);
+                    bool periodicInput = xelem.Get("periodicInput", true);
+                    int symmetry = xelem.Get("symmetry", 8);
+                    int ground = xelem.Get("ground", 0);
+
+                    model = new OverlappingModel(name, N, width, height, periodicInput, periodic, symmetry, ground, heuristic);
+                }
+                else
+                {
+                    string subset = xelem.Get<string>("subset");
+                    bool blackBackground = xelem.Get("blackBackground", false);
+
+                    model = new SimpleTiledModel(name, subset, width, height, periodic, blackBackground, heuristic);
+                }
+
+                for (int i = 0; i < xelem.Get("screenshots", 2); i++)
+                {
+                    for (int k = 0; k < 10; k++)
                     {
-                        Console.WriteLine("DONE");
-                        model.Graphics().Save($"output/{name} {seed}.png");
-                        if (model is SimpleTiledModel stmodel && xelem.Get("textOutput", false)) System.IO.File.WriteAllText($"{name} {seed}.txt", stmodel.TextOutput());
-                        break;
+                        Console.Write("> ");
+                        int seed = random.Next();
+                        bool success = model.Run(seed, xelem.Get("limit", -1));
+                        if (success)
+                        {
+                            Console.WriteLine("DONE");
+                            model.Graphics().Save($"output/{name} {seed}.png");
+                            if (model is SimpleTiledModel stmodel && xelem.Get("textOutput", false)) System.IO.File.WriteAllText($"{name} {seed}.txt", stmodel.TextOutput());
+                            break;
+                        }
+                        else Console.WriteLine("CONTRADICTION");
                     }
-                    else Console.WriteLine("CONTRADICTION");
                 }
             }
-        }
 
-        Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+            Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+        }
     }
 }
